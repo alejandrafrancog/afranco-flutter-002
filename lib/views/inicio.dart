7133bc80da77eaea199ced685ccdf8ffc5574b70
Alejandra Franco <alejandrafrancog5@gmail.com>
2025-04-03 23:32:54 -0300

    Agregar navegación entre pantallas

--- lib/views/inicio.dart
+++ lib/views/inicio.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'acerca_de.dart';
+import 'contacto.dart';
 
 class PantallaInicio extends StatelessWidget {
   @override
@@ -8,9 +10,36 @@ class PantallaInicio extends StatelessWidget {
         title: Text('Inicio'),
       ),
       body: Center(
-        child: Text(
-          '¡Bienvenido a nuestra aplicación!',
-          style: TextStyle(fontSize: 24),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '¡Bienvenido a nuestra aplicación!',
+              style: TextStyle(fontSize: 24),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              child: Text('Ir a Acerca de'),
+              onPressed: () {
+                // Navegación a pantalla Acerca de
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PantallaAcercaDe()),
+                );
+              },
+            ),
+            SizedBox(height: 15),
+            ElevatedButton(
+              child: Text('Ir a Contacto'),
+              onPressed: () {
+                // Navegación a pantalla Contacto
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PantallaContacto()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
